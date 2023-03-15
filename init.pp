e898747202bf6763f32b073832a6a03567aa0924
ShyamTalekarDevOps <127838321+ShyamTalekarDevOps@users.noreply.github.com>
2023-03-15 12:08:41 +0530

    Delete init.pp

--- init.pp
+++
@@ -1,9 +0,0 @@
-# 
-class mimecast_sea_star_server_toolkit {
-
-  class { '::mimecast_sea_star_server_toolkit::config':  }
-  class { '::mimecast_sea_star_server_toolkit::apps_config':  }
-  -> class { '::mimecast_sea_star_server_toolkit::package': }
-  class { '::mimecast_sea_star_server_toolkit::service': }
-
-}
